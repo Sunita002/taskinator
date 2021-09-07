ff03d79af588a6ea712ffcf994c7163531338dc1
sunita persad <sunitapersad020@gmail.com>
2021-09-07 18:36:06 -0400

    event form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
-var buttonEl = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form")
 console.log(buttonEl);
 
 var buttonEl = document.querySelector("#save-task"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
+
+//comment out old code to add another function on create task handler//
+/*var createTaskHandler = function() { 
   var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
+  //listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+*/
+
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task";
+    tasksToDoEl.appendChild(listItemE1);
+};
